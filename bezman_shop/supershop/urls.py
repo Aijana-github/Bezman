3ac901acfbd6bf981b0905159bbd000215ab7f88
Айжан <ayzhana.abdrahmanova@mail.ru>
2020-12-11 12:58:56 +0600

    initial commit

--- bezman_shop/supershop/urls.py
+++ bezman_shop/supershop/urls.py
@@ -9,6 +9,9 @@ urlpatterns = [
     path('orders/',orderList,name='orders'),
     path('order-create/<int:product_id>/',orderCreate,name='order-create'),
     path('order-update/<int:order_id>/',orderUpdate,name='order-update'),
+    path('task1/',task1),
+    path('order-delete/<int:order_id>/',orderDelete,name='order-delete'),
+
 
 ]
 
