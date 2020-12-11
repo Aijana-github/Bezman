f32ec80656d0412fbd2cbc5cb2f3cee4efaf0994
Айжан <ayzhana.abdrahmanova@mail.ru>
2020-12-11 12:58:46 +0600

    initial commit

--- bezman_shop/accounts/urls.py
+++ bezman_shop/accounts/urls.py
@@ -3,13 +3,19 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from .views import *
+from django.contrib.auth.views import *
 
 urlpatterns = [
-    path('customers/',customerList),
+    path('customers/',customerList,name='customer'),
+    path('reset-password/',PasswordResetView.as_view(),name='reset-password'),
+    path('reset/<uidb64>/<token>/',PasswordResetConfirmView.as_view,name='password_reset_confirm'),
+    path('reset-password-done/',PasswordResetDoneView.as_view(),name='password_reset_done'),
+    path('reset-password-complete/',PasswordResetCompleteView.as_view(),name='password_reset_complete'),
     path('customers/<int:customer_id>/',customerDetail),
     path('register/',registration),
     path('login/',auth,name='login'),
     path('logout/',logout_page,name='logout'),
+    path('reset-password/',PasswordResetView.as_view),
 
 
 ]
