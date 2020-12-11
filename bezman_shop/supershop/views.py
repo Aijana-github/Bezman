8e845323edd2e68e434182b1c78a09ef3e3f590e
Айжан <ayzhana.abdrahmanova@mail.ru>
2020-12-11 12:59:11 +0600

    initial commit

--- bezman_shop/supershop/views.py
+++ bezman_shop/supershop/views.py
@@ -1,11 +1,14 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .models import *
 from .forms import *
 from django_filters.filters import OrderingFilter
 from .filters import ProductFilter
+from .decorators import allowed_roles
 
 # Create your views here.
-
+@allowed_roles(allowed=['bezman'])
 def productList(request):
     products = Product.objects.all()
     filter = ProductFilter(request.GET,queryset=products)
@@ -13,6 +16,7 @@ def productList(request):
     context = {'products': products,'filter':filter}
     return render (request, 'supershop/products.html', context)
 
+@login_required()
 def orderList(request):
     orders = Order.objects.all()
     orders_count = orders.count()
@@ -22,7 +26,10 @@ def orderList(request):
     return render(request, 'supershop/order-List.html', context)
 
 def orderCreate(request,product_id):
-    product = Product.objects.get(id=product_id)
+    try:
+        product = Product.objects.get(id=product_id)
+    except Order.DoesNotExist:
+        return HttpResponse('page not found 404')
     customer = request.user
     form = OrdersForm(initial={'product':product,'customer':customer})
 
@@ -36,7 +43,10 @@ def orderCreate(request,product_id):
 
 
 def orderUpdate(request,order_id):
-    order = Order.objects.get(id=order_id)
+    try:
+        order = Order.objects.get(id=order_id)
+    except Order.DoesNotExist:
+        return HttpResponse('page not found 404')
     form = OrdersForm(instance=order)
     if request.method == 'POST':
         form = OrdersForm(request.POST,instance=order)
@@ -45,6 +55,50 @@ def orderUpdate(request,order_id):
             return redirect('orders')
     context = {'form':form}
     return render(request,'supershop/order-create.html',context)
+
+def task1(request):
+    ## solution
+    string = "aaaaaaaaaaaab"
+    i = set(string)
+    if len(string) % 2 == 0:
+        return HttpResponse(string)
+    else:
+        return HttpResponse('Chat with her')
+
+
+def orderDelete(request,order_id):
+    try:
+        order = Order.objects.get(id=order_id)
+    except Order.DoesNotExist:
+        return HttpResponse('No such Order.')
+    form = OrdersForm(instance=order)
+    if request.method == 'POST':
+        if order.status == 'Not Delivered':
+            order.delete()
+            return HttpResponse('Order Deleted!')
+        else:
+            return HttpResponse('Not Valid Status for delete order!')
+    context = {'order':order}
+    return render(request,'supershop/order-delete.html, context')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
